89e9e622be3fee930655d1a8ae3c0cdd52c8af21
baburam panthi <brmpanthi@gmail.com>
2018-03-05 17:18:16 +0545

    added bootstrap framework

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <html>
 <head>
 	<title>my first project</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
 <h1>welcome to my project</h1>
+<h3>courses</h3>
+<ul>
+	<li> basic course</li>
+	<li>advanced php</li>
 </body>
 </html>
